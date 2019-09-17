1603843698632c2466c9f46c423b27292ee964bc
wangpeixin7 <924791632@qq.com>
2019-09-17 15:13:02 +0800

    1.我的界面

---
+++ app/src/main/java/com/post/station/Mywallet.java
@@ -0,0 +1,16 @@
+package com.post.station;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.post.station.R;
+
+public class Mywallet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mywallet);
+    }
+}
