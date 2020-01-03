da2d8596277929b16ebc044b0bdac8c74f21ab11
wangpeixin7 <924791632@qq.com>
2020-01-03 09:30:00 +0800

    1.

---
+++ app/src/main/java/com/post/station/ui/mine/minBean.java
@@ -0,0 +1,18 @@
+package com.post.station.ui.mine;
+
+import com.post.station.response.BandBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ *  项目名： FastEightPostStation
+ *  包名： com.post.station.ui.mine
+ *  创建时间：2019/11/2610:51
+ *  作者：wpx
+ *  描述：
+ */public class minBean {
+
+
+    public static List<BandBean> listss = new ArrayList();
+}
